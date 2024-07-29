fa95302162c9ef861dd37ec6b714367b7d001867
André Kirst <github@andrekirst.de>
2024-07-29 20:52:33 +0200

    add display to init

--- src/Api/Services/InitializationService.cs
+++ src/Api/Services/InitializationService.cs
@@ -1,11 +1,26 @@
+using System.Drawing;
 using Api.Facades;
+using Api.Hardware.Displays;
+using Iot.Device.Graphics;
+using Iot.Device.Ssd13xx.Commands;
+using Iot.Device.Ssd13xx.Commands.Ssd1306Commands;
 
 namespace Api.Services;
 
-public class InitializationService(IGpioControllerFacade gpioControllerFacade) : IInitializationService
+public class InitializationService(
+    IGpioControllerFacade gpioControllerFacade,
+    Ssd1306Size128X64 display) : IInitializationService
 {
     public void Initialize()
     {
+        display.EnableDisplay(true);
+
+        using (var image = BitmapImage.CreateBitmap(128, 64, PixelFormat.Format32bppArgb))
+        {
+            image.Clear(Color.Black);
+            var g = image.GetDrawingApi();
+        }
+
         gpioControllerFacade.OpenOutputPin(PinConfiguration.Fan.Input.RelayGpioPin);
         gpioControllerFacade.OpenOutputPin(PinConfiguration.Fan.Input.FanPwmPin);
         gpioControllerFacade.SetupFanPwm(PinConfiguration.Fan.Input.FanPwmPin);
