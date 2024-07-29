4a351eae3af8b64bed3740c7d19d3be5e409b03e
André Kirst <github@andrekirst.de>
2024-07-29 20:58:49 +0200

    regsiter skiasharp

--- src/Api/Services/InitializationService.cs
+++ src/Api/Services/InitializationService.cs
@@ -2,6 +2,7 @@ using System.Drawing;
 using Api.Facades;
 using Api.Hardware.Displays;
 using Iot.Device.Graphics;
+using Iot.Device.Graphics.SkiaSharpAdapter;
 using Iot.Device.Ssd13xx.Commands;
 using Iot.Device.Ssd13xx.Commands.Ssd1306Commands;
 
@@ -13,12 +14,14 @@ public class InitializationService(
 {
     public void Initialize()
     {
+        SkiaSharpAdapter.Register();
         display.EnableDisplay(true);
-
+        const string font = "DejaVu Sans";
         using (var image = BitmapImage.CreateBitmap(128, 64, PixelFormat.Format32bppArgb))
         {
             image.Clear(Color.Black);
             var g = image.GetDrawingApi();
+            g.DrawText("Hallo", font, 25, Color.White, new Point(0, 0));
         }
 
         gpioControllerFacade.OpenOutputPin(PinConfiguration.Fan.Input.RelayGpioPin);
